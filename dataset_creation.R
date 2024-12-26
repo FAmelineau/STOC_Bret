0c303931b99c85e0696f6b54024bc39a1729d0dc
obegassat <oceane.begassat@free.fr>
2024-12-26 10:27:06 +0100

    Commit sur le script cette fois (sur le projet ce n'était peut-être pas ce qu'il fallait faire)

--- dataset_creation.R
+++ dataset_creation.R
@@ -1,29 +1,81 @@
-library(dplyr)
-library(sf)
-library(ggplot2)
+
+#Charger les packages R utiles pour la suite
+
+library(dplyr) #permet d'utiliser %>%, filter, summarize et bien d'autres.
+library(sf) #pour manipuler des objets spatiaux
+library(ggplot2)  #pour les graphiques
 
 
-
+#importer les données STOC
 mydat = read.csv2("data/0041794-241126133413365.csv", sep = '\t')
+mydat = read.csv2("D:/Home/ocbegassat/Documents/Oceane_BEGASSAT/Thesis_guifette/ENS/CPES 2 projet/0041794-241126133413365.csv", sep = '\t')
 
 
+#stocker dans une variable les données concernant uniquement les espèces spécialistes : alouette des champs et bruant jaune
 alouette = mydat %>%  filter(species == "Alauda arvensis")
 bruantj = mydat %>% filter(species == "Emberiza citrinella")
 
-
+#idem pour les espèces généralistes tourterelle des bois et mésange charbonnière
 tourt = mydat %>% filter(species == "Streptopelia turtur")
 charbo = mydat %>% filter(species == "Parus major")
 
-
+#importer une carte de l'Ille et Vilaine (polygone au format shapefile, donc un objet spatial)
+#pour les objets spatiaux utiliser st_read
 dep35 <- st_read("data/departement-35/admin-departement.shp")
 
-
+#convertir les donées STOC qui contiennent déjà des colonnes Longitude et Latitude
+#en objet spatial aussi (projeter les données)
 alouette = st_as_sf(alouette,coords = c("decimalLongitude","decimalLatitude"), crs = 4326)
-plot(alouette)
+#le crs est est le "Coordinate Reference System", ici WGS84, système géodésique mondial
+plot(alouette) #représente toutes les observations de l'alouette en France, selon leurs coordonnées
 
 
+#représenter les observations d'alouettes en Bretagne
+ggplot() + 
+  geom_sf(data = dep35) + #trace le contour du département 35
+  geom_sf(data = alouette) + #ajoute les données d'observation d'alouette 
+  coord_sf(xlim = c(-6,0), ylim = c(46,49.5), expand = FALSE) #restreint ce qu'on voit de ces données à une zone en Bretagne
+
+#représenter uniquement les observations en Ille-et-Vilaine
+#faire une intersection spatiale : extraire uniquement les points qui se trouvent dans le polygone d'intérêt (ici dpt 35)
+alouette_in_dep35 <- st_intersection(alouette, dep35)
 
 ggplot() + 
-  geom_sf(data = dep35) +
-  geom_sf(data = alouette) +
-  coord_sf(xlim = c(-6,0), ylim = c(46,49.5), expand = FALSE) 
+  geom_sf(data = dep35, fill = "white", color = "black") + # Contour du département35
+  geom_sf(data = alouette_in_dep35, color = "blue", size = 1) + # Observations d'alouettes extraites
+  theme_minimal() + #juste pour l'esthétique
+  ggtitle("Observations (STOP-eps) d'Alouettes des champs en Ille-et-Vilaine") #ajout d'un titre
+
+
+# Même chose avec les autres espèces --------------------------------------
+
+#conversion en spatial
+bruantj = st_as_sf(bruantj,coords = c("decimalLongitude","decimalLatitude"), crs = 4326)
+tourt = st_as_sf(tourt,coords = c("decimalLongitude","decimalLatitude"), crs = 4326)
+charbo = st_as_sf(charbo,coords = c("decimalLongitude","decimalLatitude"), crs = 4326)
+
+ggplot() + 
+  geom_sf(data = dep35) + #trace le contour du département 35
+  geom_sf(data= charbo, color = "grey")+
+  geom_sf(data = alouette, color = "blue") + #ajoute les données d'observation d'alouette 
+  geom_sf(data= bruantj, color = "yellow")+
+  geom_sf(data= tourt, color = "brown")+
+  coord_sf(xlim = c(-6,0), ylim = c(46,49.5), expand = FALSE) #restreint ce qu'on voit de ces données à une zone en Bretagne
+
+bruantj_in_dep35 <- st_intersection(bruantj, dep35)
+tourt_in_dep35 <- st_intersection(tourt, dep35)
+charbo_in_dep35 <- st_intersection(charbo, dep35)
+
+ggplot() + 
+  geom_sf(data = dep35, fill = "white", color = "black") + # Contour du département
+  geom_sf(data = charbo_in_dep35, aes(color = "Mésange charbonnière"), size = 1) + # Points pour mésange charbonnière
+  geom_sf(data = alouette_in_dep35, aes(color = "Alouette des champs"), size = 1) + # Points pour alouette
+  geom_sf(data = bruantj_in_dep35, aes(color = "Bruant jaune"), size = 1) + # Points pour bruant jaune
+  geom_sf(data = tourt_in_dep35, aes(color = "Tourterelle des bois"), size = 1) + # Points pour tourterelle
+  scale_color_manual(values = c("Mésange charbonnière" = "grey", #pour ajouter une légende manuellement
+                                "Alouette des champs" = "blue", 
+                                "Bruant jaune" = "yellow", 
+                                "Tourterelle des bois" = "brown")) + # Couleurs spécifiques pour chaque espèce
+  theme_minimal() + #pour l'esthétique
+  ggtitle("Observations des espèces (STOC-EPS) en Ille-et-Vilaine")+
+  theme(legend.title = element_blank()) # Enlever le titre de la légende
