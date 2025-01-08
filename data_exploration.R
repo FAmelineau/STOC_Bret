0edfd46015616e33c41cbce3f9da777298ebcd6f
obegassat <oceane.begassat@free.fr>
2025-01-08 11:12:15 +0100

    Scripts ré-annotés et complétés

--- data_exploration.R
+++ data_exploration.R
@@ -10,29 +10,36 @@ library(dplyr) #permet d'utiliser %>%, filter, summarize et bien d'autres.
 library(sf) #pour manipuler des objets spatiaux
 library(ggplot2)  #pour les graphiques
 
+#ré-importer les jeux de données enregistrés
 alouette.bret = readRDS("data/alouette.bret.RDS")
-
 bruantj.bret = readRDS( "data/bruantj.bret.RDS")
 
-sum.alouette = alouette.bret %>% 
-  st_drop_geometry() %>% 
-  group_by(year) %>% 
-  summarize(n.observations = n(), n.observateurs = n_distinct(recordedBy) ) %>% 
-  mutate(ratio = n.observations/n.observateurs)
 
+
+# Nombre d'observations et d'observateurs d'Alouettes ---------------------
+
+#Somme des observateurs et observations d'alouettes par an et ratio de l'un sur l'autre
+sum.alouette = alouette.bret %>% 
+  st_drop_geometry() %>% #retire la partie spatial epour faciliter le traitement des données numériques
+  group_by(year) %>%  #groupe par année (première colone = année)
+  summarize(n.observations = n(), n.observateurs = n_distinct(recordedBy) ) %>% #crée deux nouvelles colonnes
+#nombre d'observations = nombre de ligne alouette par an
+#nombre d'observateurs = nombre de noms différents ayant fait ces observations d'alouettes par an
+  mutate(ratio = n.observations/n.observateurs) #nouvelle colonne qui résume le ratio des deux
+
+# représenter ce ratio selon les années
 ggplot(sum.alouette) +
   geom_point(aes(x=year, y = ratio))
 
 
 
+# Même chose pour le bruant jaune -----------------------------------------
 
 sum.bruantj = bruantj.bret %>% 
   st_drop_geometry() %>% 
   group_by(year) %>% 
   summarize(n.observations = n(), n.observateurs = n_distinct(recordedBy) ) %>% 
   mutate(ratio = n.observations/n.observateurs)
-
-
 
 ggplot(sum.bruantj) +
   geom_point(aes(x=year, y = ratio))
--- dataset_creation.R
+++ dataset_creation.R
@@ -14,7 +14,6 @@ library(ggplot2)  #pour les graphiques
 
 #importer les données STOC
 mydat = read.csv2("data/0041794-241126133413365.csv", sep = '\t') # note pour Océane: pense à mettre le fichier de données dans le dossier STOC_Bret/data !
-mydat = read.csv2("D:/Home/ocbegassat/Documents/Oceane_BEGASSAT/Thesis_guifette/ENS/CPES 2 projet/0041794-241126133413365.csv", sep = '\t') # ligne à enlever
 
 
 #stocker dans une variable les données concernant uniquement les espèces spécialistes : alouette des champs et bruant jaune
@@ -29,23 +28,27 @@ charbo = mydat %>% filter(species == "Parus major")
 #pour les objets spatiaux utiliser st_read
 dep35 <- st_read("data/departement-35/admin-departement.shp")
 
+
+# Représenter les points pour l'alouette des champs -----------------------
+
 #convertir les donées STOC qui contiennent déjà des colonnes Longitude et Latitude
-#en objet spatial aussi (projeter les données)
+#en objet spatial aussi (les données Lon Lat sont reconnues grâce au système de coordonnées)
 alouette = st_as_sf(alouette,coords = c("decimalLongitude","decimalLatitude"), crs = 4326)
 #le crs est est le "Coordinate Reference System", ici WGS84, système géodésique mondial
-plot(st_geometry(alouette)) #représente toutes les observations de l'alouette en France, selon leurs coordonnées
+plot(st_geometry(alouette)) #représente toutes les observations de l'alouette en France, selon leurs coordonnées (st_geometry pour ne représenter les points qu'une fois, selon la colonne geometry uniquement)
 
 
 #représenter les observations d'alouettes en Bretagne
 ggplot() + 
   geom_sf(data = dep35) + #trace le contour du département 35
-  geom_sf(data = alouette) + #ajoute les données d'observation d'alouette 
-  coord_sf(xlim = c(-6,0), ylim = c(46,49.5), expand = FALSE) #restreint ce qu'on voit de ces données à une zone en Bretagne
+  geom_sf(data = alouette) + #ajoute les données d'observation d'alouettes
+  coord_sf(xlim = c(-6,0), ylim = c(46,49.5), expand = FALSE) #restreint ce qu'on voit de ces données à une zone en Bretagne (grâce aux degrés N et W)
 
 #représenter uniquement les observations en Ille-et-Vilaine
 #faire une intersection spatiale : extraire uniquement les points qui se trouvent dans le polygone d'intérêt (ici dpt 35)
 alouette_in_dep35 <- st_intersection(alouette, dep35)
 
+#Tracer
 ggplot() + 
   geom_sf(data = dep35, fill = "white", color = "black") + # Contour du département35
   geom_sf(data = alouette_in_dep35, color = "blue", size = 1) + # Observations d'alouettes extraites
@@ -60,6 +63,7 @@ bruantj = st_as_sf(bruantj,coords = c("decimalLongitude","decimalLatitude"), crs
 tourt = st_as_sf(tourt,coords = c("decimalLongitude","decimalLatitude"), crs = 4326)
 charbo = st_as_sf(charbo,coords = c("decimalLongitude","decimalLatitude"), crs = 4326)
 
+#représenter toutes les observations en Bretagne
 ggplot() + 
   geom_sf(data = dep35) + #trace le contour du département 35
   geom_sf(data= charbo, color = "grey")+
@@ -68,10 +72,12 @@ ggplot() +
   geom_sf(data= tourt, color = "brown")+
   coord_sf(xlim = c(-6,0), ylim = c(46,49.5), expand = FALSE) #restreint ce qu'on voit de ces données à une zone en Bretagne
 
+#extraire les données en Ille-et-Vilaine
 bruantj_in_dep35 <- st_intersection(bruantj, dep35)
 tourt_in_dep35 <- st_intersection(tourt, dep35)
 charbo_in_dep35 <- st_intersection(charbo, dep35)
 
+#représneter
 ggplot() + 
   geom_sf(data = dep35, fill = "white", color = "black") + # Contour du département
   geom_sf(data = charbo_in_dep35, aes(color = "Mésange charbonnière"), size = 1) + # Points pour mésange charbonnière
@@ -89,32 +95,36 @@ ggplot() +
 
 
 # Créer un polygone Bretagne --------------------------------------
+
+#en rassemblant les différents polygines des départements
 dep35 <- st_read("data/departement-35/admin-departement.shp")
 dep22 <- st_read("data/departement-22/admin-departement.shp")
 dep56 <- st_read("data/departement-56/admin-departement.shp")
 dep29 <- st_read("data/departement-29/admin-departement.shp")
 
+#rassembler les polygones deux à deux pour obtenir un seul polygone
 dep35_22 = st_union(dep35,dep22)
 plot(st_geometry(dep35_22))
 
 dep56_29 = st_union(dep56,dep29)
 plot(st_geometry(dep56_29))
 
+#rassembler les deux polygones qui en résultent pour obtenir la Bretagne
 Bret = st_union(dep35_22,dep56_29)
 plot(st_geometry(Bret))
 
 
-# Extraire observations bretonnes  --------------------------------------
+# Extraire les observations bretonnes  --------------------------------------
 
-
+#pour l'alouette
 alouette.bret = st_intersection(alouette,Bret)
 ggplot() + 
   geom_sf(data = Bret, fill = "white", color = "black") + # Contour du département35
-  geom_sf(data = bruantj.bret, color = "blue", size = 1) + # Observations d'alouettes extraites
+  geom_sf(data = alouette.bret, color = "blue", size = 1) + # Observations d'alouettes extraites
   theme_minimal() + #juste pour l'esthétique
   ggtitle("Observations (STOP-eps) d'alouettes des champs en Bretagne") #ajout d'un titre
 
-
+#pour le bruant
 bruantj.bret = st_intersection(bruantj,Bret)
 ggplot() + 
   geom_sf(data = Bret, fill = "white", color = "black") + # Contour du département35
@@ -122,23 +132,24 @@ ggplot() +
   theme_minimal() + #juste pour l'esthétique
   ggtitle("Observations (STOP-eps) de Bruants jaunes en Bretagne") #ajout d'un titre
 
+#pour la mésange
+charbo.bret = st_intersection(charbo,Bret)
+ggplot() + 
+  geom_sf(data = Bret, fill = "white", color = "black") + # Contour du département35
+  geom_sf(data = charbo.bret, color = "blue", size = 1) + # Observations d'alouettes extraites
+  theme_minimal() + #juste pour l'esthétique
+  ggtitle("Observations (STOP-eps) de Bruants jaunes en Bretagne") #ajout d'un titre
 
-
-
-
-
-# toutes les espèces en Bretagne
+# Dataset avec toutes les espèces en Bretagne
 
 data.bret = mydat %>% 
   st_as_sf(coords = c("decimalLongitude","decimalLatitude"), crs = 4326) %>% 
   st_intersection(Bret)
 
-
-
-
-
-saveRDS(data.bret, "data/data.bret.RDS")
-write.csv2(data.bret, "data/data.bret.csv")
+#Sauvegarder les données dans les documents
+saveRDS(data.bret, "data/data.bret.RDS") 
+#format utile en programmation R car compresse les données (prend moins de place), conserve les métadonnées (le type des données, classes, attributs...) et s'ouvre facilement
+write.csv2(data.bret, "data/data.bret.csv") #pour ouvrir les données sous excel
 
 saveRDS(alouette.bret, "data/alouette.bret.RDS")
 write.csv2(alouette.bret, "data/alouette.bret.csv")
@@ -146,8 +157,22 @@ write.csv2(alouette.bret, "data/alouette.bret.csv")
 saveRDS(bruantj.bret, "data/bruantj.bret.RDS")
 write.csv2(bruantj.bret, "data/bruantj.bret.csv")
 
-# annoter les nouvelles lignes
-# rajouter la carte interactive
 
+# Pour le fun : créer une carte interactive -------------------------------
 
+#nécessite un nouveau package
+library(tmap)
+
+#définir le mode interactif
+tmap_mode("view")
+
+tm_shape(charbo.bret) + 
+  tm_dots(col = "grey", size = 0.1)+
+  tm_shape(bruantj.bret) + 
+  tm_dots(col = "yellow", size = 0.1)+ #trace le spoints, size définit leur taille
+  tm_shape(alouette.bret) + 
+  tm_dots(col = "brown", size = 0.1)+
+  tm_layout(title = "Observations des espèces (STOC-EPS) en Bretagne",
+            legend.show = TRUE,
+            legend.outside = TRUE)
 
